93cc1a1281324adad29f80da286108c4b23ed158
Exper1ence <3533357503@qq.com>
2016-12-10 23:37:58 +0800

    update

--- index.js
+++ index.js
@@ -3,19 +3,17 @@
  */
 const Path = require('path');
 
-module.exports = {
-    Rest(route, dir, restDir = '_rest'){
-        const router = require('express').Router();
-        const subRouters = require('dynamic-object-generator')(dir, true);
-        const restRouters = require('dynamic-object-generator')(Path.resolve(dir, restDir), true);
-        
-        for (let restRouter of restRouters) {
-            router.use(route, restRouter);
-        }
-        for (let subRouter of subRouters) {
-            router.use(route, subRouter);
-        }
-        
-        return router;
-    },
+module.exports = (route, dir, methodsDirname = '_rest') => {
+    const router = require('express').Router();
+    const subRouters = require('dynamic-object-generator')(dir, true);
+    const restRouters = require('dynamic-object-generator')(Path.resolve(dir, methodsDirname), true);
+    
+    for (let restRouter of restRouters) {
+        router.use(route, restRouter);
+    }
+    for (let subRouter of subRouters) {
+        router.use(route, subRouter);
+    }
+    
+    return router;
 };
