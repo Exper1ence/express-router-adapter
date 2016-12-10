97840e6ae933e5d03f221175ce0b58c38eb0faf1
Exper1ence <3533357503@qq.com>
2016-12-10 22:59:31 +0800

    v0.1.0

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 /**
  * Created by Exper1ence on 2016/12/10.
  */
+const Path = require('path');
+
+module.exports = {
+    Rest(route, dir, restDir = '_rest'){
+        const router = require('express').Router();
+        const subRouters = require('dynamic-object-generator')(dir, true);
+        const restRouters = require('dynamic-object-generator')(Path.resolve(dir, restDir), true);
+        
+        for (let restRouter of restRouters) {
+            router.use(route, restRouter);
+        }
+        for (let subRouter of subRouters) {
+            router.use(route, subRouter);
+        }
+        
+        return router;
+    },
+};
